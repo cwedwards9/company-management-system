4b002604acff8d2482e3406388a2881af986aeda
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-10 14:03:50 -0500

    Change order of tables as they are being created.

--- schema.sql
+++ schema.sql
@@ -2,28 +2,26 @@ DROP DATABASE IF EXISTS company;
 CREATE DATABASE company;
 USE company;
 
-CREATE TABLE employee (
-    employee_id INT AUTO_INCREMENT PRIMARY_KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERENCES employee (employee_id) ON DELETE CASCADE
+CREATE TABLE department (
+    dept_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
-
 CREATE TABLE role (
-    role_id INT AUTO_INCREMENT PRIMARY_KEY,
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(8,2),
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department (dept_id) ON DELETE CASCADE
 );
 
-
-CREATE TABLE department (
-    dept_id INT AUTO_INCREMENT PRIMARY_KEY,
-    name VARCHAR(30)
+CREATE TABLE employee (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee (employee_id) ON DELETE CASCADE
 );
 
