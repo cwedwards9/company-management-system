fe9751678363ec505c2399069e17024e68086de5
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-10 16:22:19 -0500

    Update employee table, add default of null to manager_id column.

--- schema.sql
+++ schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
+    manager_id INT DEFAULT NULL,
     FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE CASCADE,
     FOREIGN KEY (manager_id) REFERENCES employee (employee_id) ON DELETE CASCADE
 );
