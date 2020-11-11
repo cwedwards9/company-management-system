2558fcad9db9f6762804608c3afd34b976abc735
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-11 14:51:32 -0500

    Change the insertion of data to make data more random within the employee table.

--- seed.sql
+++ seed.sql
@@ -1,3 +1,5 @@
+-- Pre-populated data inserted into each table
+
 INSERT INTO department (name) VALUES 
 ("Finance"),
 ("Software Dev."),
@@ -16,16 +18,25 @@ INSERT INTO role (title, salary, department_id) VALUES
 ("Accounting Manager", 90000.00, 4)
 ;
 
--- Insert managers (with no FK for manager_id)
-INSERT INTO employee (first_name, last_name, role_id) VALUES
-('Keaton', 'Adams', 4),
-('Brandt', 'Kunze', 8),
-('Benny', 'McLaughlin', 2),
-('Sandy', 'Miller', 6);
+-- Managers and non-managers were inserted separately to take into account that managers do not have a manager so they
+-- do not have a manager_id Foreign Key
+
+-- Each employee was also given an employee_id manually even though there is an auto_increment set up in the employee
+-- table schema. This is purposely done so that the table is random and it does not list all the managers and then all
+-- of the employees
+
+-- Insert managers (with no FK, manager_id)
+INSERT INTO employee (employee_id, first_name, last_name, role_id) VALUES
+(8, 'Keaton', 'Adams', 4),
+(4, 'Brandt', 'Kunze', 8),
+(1, 'Benny', 'McLaughlin', 2),
+(5, 'Sandy', 'Miller', 6);
+
 
 -- Insert employees (with a FK for manager_id)
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
-('Beau', 'Feeney', 1, 3),
-('Leonor', 'Gerlach', 7, 2),
-('Meggie', 'Weissnat', 3, 1),
-('Lolita', 'Ruecker', 5, 4);
+-- **NOTE: It is important that the employees' managers are managers in the same department that the employees are being assigned to
+INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id) VALUES 
+(2, 'Beau', 'Feeney', 1, 1),
+(7, 'Leonor', 'Gerlach', 7, 4),
+(6, 'Meggie', 'Weissnat', 3, 8),
+(3, 'Lolita', 'Ruecker', 5, 5);
