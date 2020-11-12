cf817f0af83256bb7a876905ae98224de3d5bb1f
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-12 10:18:10 -0500

    Move db connection and queries to own separate files.

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
+const Department = require("./lib/Department");
+const Role = require("./lib/Role");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
-const mysql = require("mysql");
 const promptList = require("./promptList");
-
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    port: 3306,
-    database: "company"
-});
+const query = require("./config/queries");
+const connection = require("./config/connection");
 
 
 function init() {
+    // Gives user first prompt (from promptList.js)
     inquirer.prompt(promptList[0])
     .then(answer => {
         if(answer.mainFunctions === "View All Employees") {
-            const q = viewAllEmployees();
+            const q = query.viewAllEmployees();
             connection.query(q, (err, result) => {
                 if(err) throw err;
                 // console.log(result);
@@ -25,50 +21,37 @@ function init() {
             })
         }
         else if(answer.mainFunctions === "View All Departments") {
-            const q = "SELECT dept_id AS id, 'department name' FROM department;"
+            const q = query.viewAllDepts();
             connection.query(q, (err, result) => {
                 if(err) throw err;
                 console.table("Departments:", result);
             })
         }
         else if(answer.mainFunctions === "View All Roles") {
-            const q = viewAllRoles();
+            const q = query.viewAllRoles();
             connection.query(q, (err, result) => {
                 if(err) throw err;
                 console.table("Roles:", result);
             })
         }
         else if(answer.mainFunctions === "Add Department") {
-            inquirer.prompt(promptList[1])
+            addDepartment();
         }
     });
+
+
+    function addDepartment() {
+        inquirer.prompt(promptList[1])
+        .then(answer => {
+            const { newDepartment } = answer;
+
+            const department = new Department(name);
+        })
+    }
+
+
 }
 
 
 // Initialize the application to prompt the user
 init();
-
-
-
-// QUERIES
-function viewAllEmployees() {
-    return `SELECT e.employee_id AS id, e.first_name, e.last_name,
-            role.title, role.salary,
-            department.name AS department,
-            CONCAT(m.first_name, " ", m.last_name) AS manager
-            FROM employee e
-            LEFT JOIN employee m
-            ON e.manager_id = m.employee_id
-            INNER JOIN role
-            ON e.role_id = role.role_id
-            INNER JOIN department
-            ON role.department_id = department.dept_id
-            ORDER BY e.employee_id;`;
-}
-
-function viewAllRoles() {
-    return `SELECT r.role_id AS id, r.title, r.salary, d.name AS department
-            FROM role r
-            INNER JOIN department d
-            ON r.department_id = d.dept_id;`;
-}
