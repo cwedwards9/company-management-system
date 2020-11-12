46959758c5c5ec8df907ba85a6e7cc51e7d16ff5
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-12 13:35:32 -0500

    Finish addDeptartment function to insert a new dept into department table. Create a new function, addRole(), for inserting a new role into the role table.

--- app.js
+++ app.js
@@ -9,47 +9,89 @@ const connection = require("./config/connection");
 
 
 function init() {
-    // Gives user first prompt (from promptList.js)
-    inquirer.prompt(promptList[0])
-    .then(answer => {
-        if(answer.mainFunctions === "View All Employees") {
-            const q = query.viewAllEmployees();
-            connection.query(q, (err, result) => {
-                if(err) throw err;
-                // console.log(result);
-                console.table("Employees:", result);
-            })
-        }
-        else if(answer.mainFunctions === "View All Departments") {
-            const q = query.viewAllDepts();
-            connection.query(q, (err, result) => {
-                if(err) throw err;
-                console.table("Departments:", result);
-            })
-        }
-        else if(answer.mainFunctions === "View All Roles") {
-            const q = query.viewAllRoles();
-            connection.query(q, (err, result) => {
-                if(err) throw err;
-                console.table("Roles:", result);
-            })
-        }
-        else if(answer.mainFunctions === "Add Department") {
-            addDepartment();
-        }
-    });
-
-
-    function addDepartment() {
-        inquirer.prompt(promptList[1])
+    function mainMenu() {
+        // Gives user first prompt (from promptList.js)
+        inquirer.prompt(promptList[0])
         .then(answer => {
-            const { newDepartment } = answer;
+            if(answer.mainFunctions === "View All Employees") {
+                const q = query.viewAllEmployees();
+                connection.query(q, (err, result) => {
+                    if(err) throw err;
+                    // console.log(result);
+                    console.table("Employees:", result);
+                })
+            }
+            else if(answer.mainFunctions === "View All Departments") {
+                const q = query.viewAllDepts();
+                connection.query(q, (err, result) => {
+                    if(err) throw err;
+                    console.log(result);
+                    console.table("Departments:", result);
+                })
+            }
+            else if(answer.mainFunctions === "View All Roles") {
+                const q = query.viewAllRoles();
+                connection.query(q, (err, result) => {
+                    if(err) throw err;
+                    console.table("Roles:", result);
+                })
+            }
+            else if(answer.mainFunctions === "Add Role") {
+                addRole();
+            }
+            else if(answer.mainFunctions === "Add Department") {
+                addDepartment();
+            }
+        });
 
-            const department = new Department(name);
-        })
+        function addRole() {
+            inquirer.prompt(promptList[1])
+                .then(answer => {
+                    const { newRoleTitle, newRoleSalary, newRoleDept } = answer;
+
+                    // Based on department name chosen, we must dynamically get the corresponding id to insert into the table
+                    var deptNumber;
+                    const q1 = query.viewAllDepts();
+                    connection.query(q1, (err, result) => {
+                        if(err) throw err;
+                        for(let i = 0; i < result.length; i++){
+                            if(newRoleDept === result[i].name){
+                                deptNumber = result[i].id;
+                                
+                                // Create new instance of role, insert new role into role table with the deptartment id
+                                const role = new Role(newRoleTitle, newRoleSalary, deptNumber);
+                                const q2 = query.createRole();
+                                connection.query(q2, role, (err, result) => {
+                                    if(err) throw err;
+                                    console.log(role.title + " was successfully inserted into the role table!");
+                                });
+                            }
+                        }
+                    });
+        
+                    mainMenu();
+                })
+        }
+
+        function addDepartment() {
+            inquirer.prompt(promptList[2])
+            .then(answer => {
+                const { newDepartment } = answer;
+
+                const department = new Department(newDepartment);
+                console.log(department);
+                const q = query.createDept();
+                connection.query(q, department, (err, result) => {
+                    if(err) throw err;
+                    console.log(department.name + " was successfully inserted into the department table!");
+                });
+
+                mainMenu();
+            });
+        }
     }
 
-
+    mainMenu();
 }
 
 
