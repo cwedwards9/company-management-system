f22505b6a1db8b1b35110d3e94583607d999ae5f
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-15 15:03:18 -0500

    Refactor insert statement.

--- db/seed.sql
+++ db/seed.sql
@@ -18,25 +18,18 @@ INSERT INTO role (title, salary, department_id) VALUES
 ("Accounting Manager", 90000.00, 4)
 ;
 
--- Managers and non-managers were inserted separately to take into account that managers do not have a manager so they
--- do not have a manager_id Foreign Key
 
 -- Each employee was also given an employee_id manually even though there is an auto_increment set up in the employee
 -- table schema. This is purposely done so that the table is random and it does not list all the managers and then all
 -- of the employees
 
--- Insert managers (with no FK, manager_id)
-INSERT INTO employee (employee_id, first_name, last_name, role_id) VALUES
-(8, 'Keaton', 'Adams', 4),
-(4, 'Brandt', 'Kunze', 8),
-(1, 'Benny', 'McLaughlin', 2),
-(5, 'Sandy', 'Miller', 6);
-
-
--- Insert employees (with a FK for manager_id)
 -- **NOTE: It is important that the employees' managers are managers in the same department that the employees are being assigned to
 INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id) VALUES 
 (2, 'Beau', 'Feeney', 1, 1),
+(1, 'Benny', 'McLaughlin', 2, NULL),
 (7, 'Leonor', 'Gerlach', 7, 4),
+(4, 'Brandt', 'Kunze', 8, NULL),
 (6, 'Meggie', 'Weissnat', 3, 8),
-(3, 'Lolita', 'Ruecker', 5, 5);
+(8, 'Keaton', 'Adams', 4, NULL),
+(3, 'Lolita', 'Ruecker', 5, 5),
+(5, 'Sandy', 'Miller', 6, NULL);
